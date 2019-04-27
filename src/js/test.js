1e7787b5193f5242bdbac1b895c043d56d6084a3
Maksim <maaa23@mail.ru>
2019-04-27 11:54:51 +0400

    add blog

---
+++ src/js/test.js
@@ -0,0 +1,68 @@
+/* eslint-disable linebreak-style */
+import $ from 'jquery';
+
+$('document').ready(
+  () => $('.button[filter="wd"]').click(
+    () => {
+      if ($('.button[filter="wd"]').attr('val') === 'off') {
+        $('.button[filter]').attr('val', 'off');
+        $('.button[filter]').removeClass('focused');
+        $('.button[filter="wd"]').addClass('focused');
+        $('.button[filter="wd"]').attr('val', 'on');
+        $('.services-types img').hide(300);
+        $('img[filter="wd"]').show(300);
+      }
+    },
+  ),
+);
+$('document').ready(
+  () => $('.button[filter="ud"]').click(
+    () => {
+      if ($('.button[filter="ud"]').attr('val') === 'off') {
+        $('.button[filter]').attr('val', 'off');
+        $('.button[filter]').removeClass('focused');
+        $('.button[filter="ud"]').addClass('focused');
+        $('.button[filter="ud"]').attr('val', 'on');
+        $('.portfolio-items img').hide(300);
+        $('img[filter="ud"]').show(300);
+      }
+    },
+  ),
+);
+$('document').ready(
+  () => $('.button[filter="moc"]').click(
+    () => {
+      if ($('.button[filter="moc"]').attr('val') === 'off') {
+        $('.button[filter]').attr('val', 'off');
+        $('.button[filter="moc"]').attr('val', 'on');
+        $('.button[filter]').removeClass('focused');
+        $('.button[filter="moc"]').addClass('focused');
+        $('.portfolio-items img').hide(300);
+        $('img[filter="moc"]').show(300);
+      }
+    },
+  ),
+);
+
+$('document').ready(
+  () => $('.button[filter="all"]').click(
+    () => {
+      $('.button[filter]').attr('val', 'off');
+      $('.button[filter="all"]').attr('val', 'on');
+      $('.button[filter]').removeClass('focused');
+      $('.button[filter="all"]').addClass('focused');
+      $('.portfolio-items div img').show(300);
+    },
+  ),
+);
+$('document').ready(
+  () => $('.button[filter="view-all"]').click(
+    () => {
+      $('.button[filter]').attr('val', 'off');
+      $('.button[filter="view-all"]').attr('val', 'on');
+      $('.button[filter]').removeClass('focused');
+      $('.button[filter="view-all"]').addClass('focused');
+      $('.portfolio-items div img').show(300);
+    },
+  ),
+);
