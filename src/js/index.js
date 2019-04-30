ceb9a1aedcaafb3ac6247d0485182571e3515ab9
Maksim <maaa23@mail.ru>
2019-04-30 22:38:39 +0400

    setting responsive

--- src/js/index.js
+++ src/js/index.js
@@ -80,10 +80,18 @@ $('document').ready(
     dotsClass: 'dots-style',
     responsive: [
       {
-        breakpoint: 1200,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
